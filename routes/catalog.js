c923404bd047258f8d835c7d657081aa8ace0011
Karol <krlpl06@gmail.com>
2020-06-08 20:33:03 +0200

    code cleaning, comments

--- routes/catalog.js
+++ routes/catalog.js
@@ -1,51 +1,56 @@
 const express = require('express');
 const router = express.Router();
 
-const card_controller = require('../controllers/card_controller')
-const dictonary_controller = require('../controllers/dictonary_controller')
+const CardController = require('../controllers/card_controller')
+const DictonaryController = require('../controllers/dictonary_controller')
 
 //home page
-router.get('/', dictonary_controller.dictonaryList);
+router.get('/', DictonaryController.dictonaryList);
+
 
 //DICTONARY ROUTES
 
 //dict create get
-router.get('/dictonary/create', dictonary_controller.dictonaryCreateGet);
+router.get('/dictonary/create', DictonaryController.dictonaryCreateGet);
 
 //dict create post
-router.post('/dictonary/create', dictonary_controller.dictonaryCreatePost);
+router.post('/dictonary/create', DictonaryController.dictonaryCreatePost);
 
 //dict update get
-router.get('/dictonary/update/:id', dictonary_controller.dictonaryUpdateGet);
+router.get('/dictonary/update/:id', DictonaryController.dictonaryUpdateGet);
 
 //dict update post
-router.post('/dictonary/update/:id', dictonary_controller.dictonaryUpdatePost);
-
-//dict delete get
-// router.get('/dictonary/delete/:id', dictonary_controller.dictonary_delete_get);
+router.post('/dictonary/update/:id', DictonaryController.dictonaryUpdatePost);
 
 //dict delete post
-router.post('/dictonary/delete/:id', dictonary_controller.dictonaryDeletePost);
+router.post('/dictonary/delete/:id', DictonaryController.dictonaryDeletePost);
 
 //show one dict get
-router.get('/dictonary/:id', dictonary_controller.dictonary);
+router.get('/dictonary/:id', DictonaryController.dictonary);
+
 
 //CARD ROUTES
 
-router.get('/dictonary/:id/card/create', card_controller.cardCreateGet);
+//card create get
+router.get('/dictonary/:id/card/create', CardController.cardCreateGet);
 
-router.post('/dictonary/:id/card/create', card_controller.cardCreatePost);
+//card create post
+router.post('/dictonary/:id/card/create', CardController.cardCreatePost);
 
-router.get('/dictonary/:id/card/:card/update', card_controller.cardUpdateGet);
+//card update get
+router.get('/dictonary/:id/card/:card/update', CardController.cardUpdateGet);
 
-router.post('/dictonary/:id/card/:card/update', card_controller.cardUpdatePost);
+//card update post
+router.post('/dictonary/:id/card/:card/update', CardController.cardUpdatePost);
 
-
-router.post('/dictonary/:id/card/:card/delete', card_controller.cardDeletePost);
+//card delete
+router.post('/dictonary/:id/card/:card/delete', CardController.cardDeletePost);
 
 //all cards from dictonary
-router.get('/dictonary/:id/card_list', card_controller.cardList);
+router.get('/dictonary/:id/card_list', CardController.cardList);
 
-router.get('/card/:id', card_controller.card);
+//one card
+router.get('/card/:id', CardController.card);
+
 
 module.exports = router
